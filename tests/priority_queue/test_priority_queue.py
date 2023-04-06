03fdd17c65f3ec5b7ebcf006fc77d766ebd34112
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2023-04-06 11:59:19 -0300

    requisito 6 ao 8

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,43 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+mock_high_priorit = {
+    "nome_do_arquivo": "arquivo_teste.txt",
+    "qtd_linhas": 3,
+    "linhas_do_arquivo": [""],
+}
+
+mock_low_priorit = {
+    "nome_do_arquivo": "arquivo_teste.txt",
+    "qtd_linhas": 9,
+    "linhas_do_arquivo": [""],
+}
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+
+    queue.enqueue(mock_high_priorit)
+    queue.enqueue(mock_low_priorit)
+
+    assert len(queue) == 2
+
+    priority = queue.is_priority(mock_low_priorit)
+    assert priority is False
+
+    result = queue.search(0)
+    assert result["qtd_linhas"] == 3
+
+    result = queue.search(1)
+    assert result["qtd_linhas"] == 9
+
+    queue.dequeue()
+    assert len(queue) == 1
+
+    result = queue.search(0)
+    print(result)
+    assert result["qtd_linhas"] == 9
+    assert result == mock_low_priorit
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(99)
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,52 @@
+def find_word_in_lines(word, file):
+    ocorrencias = []
+
+    for idx, line in enumerate(file["linhas_do_arquivo"]):
+        if word.lower() in line.lower():
+            ocorrencias.append(
+                {
+                    "linha": idx + 1,
+                    "conteudo": line
+                }
+            )
+
+    return ocorrencias
+
+
+def get_word_occurrences_in_files(word, instance, type):
+    search = {
+        "palavra": word,
+        "arquivo": "",
+        "ocorrencias": []
+    }
+
+    all_word_searched = []
+
+    # O(n²)
+    for index in range(0, len(instance)):
+        file = instance.search(index)
+        search["arquivo"] = file["nome_do_arquivo"]
+        if type == 'exists':
+            search["ocorrencias"] = [
+                {"linha": line["linha"]}
+                for line in find_word_in_lines(word, file)
+            ]
+        elif type == 'search':
+            search["ocorrencias"] = find_word_in_lines(word, file)
+
+        if len(search["ocorrencias"]) > 0:
+            all_word_searched.append(search)
+
+    return all_word_searched
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    return get_word_occurrences_in_files(
+        word=word, instance=instance, type='exists'
+    )
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return get_word_occurrences_in_files(
+        word=word, instance=instance, type='search'
+    )
